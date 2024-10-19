9e74f0a72988384407c550d8e7477f40599ef81a
Angel <maldonadoa1@ufl.edu>
2024-10-19 11:45:15 -0400

    Link from login to other pages

--- adv-journal/src/App.js
+++ adv-journal/src/App.js
@@ -2,11 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LoginForm from './Components/LoginForm/LoginForm';
+import Links from './Components/Links';
+import Page1 from './Components/Pages/page1';
+import Page2 from './Components/Pages/page2';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <LoginForm />
+    <div className="App">
+      <Links />
+      <Routes>
+        <Route path="/loginform" element={<LoginForm />} />
+        <Route path="/page1" element={<Page1 />} />
+        <Route path="/page2" element={<Page2 />} />
+      </Routes>
     </div>
   );
 }
---
+++ adv-journal/src/Components/Pages/page1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Page1 = () => {
+  return (
+    <div className="page">
+      <h2>Page 1</h2>
+      <p>This is the first page. Maybe homepage or profile creation page.</p>
+    </div>
+  );
+};
+
+export default Page1;
