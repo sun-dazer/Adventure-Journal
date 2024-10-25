6b9202351f46fe23b07d6935717279e71b39727b
Angel <maldonadoa1@ufl.edu>
2024-10-25 18:50:22 -0400

    Added simple forgot password

---
+++ frontend/src/Pages/ForgotPassword/forgotPassword.js
@@ -0,0 +1,39 @@
+// ForgotPassword.js
+import React, { useState } from 'react';
+import './forgotPassword.css';
+
+const ForgotPassword = () => {
+  const [username, setUsername] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSubmitted(true);
+  };
+
+  return (
+    <div className="ForgotPassword">
+      <h1>Forgot Password</h1>
+      {!submitted ? (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="username">Enter your username:</label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit">Reset Password</button>
+        </form>
+      ) : (
+        // {username} to display users's username
+        <p>Password reset process goes here? Login Again! </p>
+      )}
+    </div>
+  );
+};
+
+export default ForgotPassword;
